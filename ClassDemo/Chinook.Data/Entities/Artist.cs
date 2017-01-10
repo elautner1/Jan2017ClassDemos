0c82b3e6322b299d1851431d1668760576bfff0c
elautner1 <elautner1@studentmail.nait.ca>
2017-01-09 19:02:59 -0700

    Added classes and stuff

---
+++ ClassDemo/Chinook.Data/Entities/Artist.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region additional Namespaces
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+#endregion
+
+namespace Chinook.Data.Entities
+{
+
+    [Table("Artists")]
+   public class Artist
+    {
+        [Key]
+        public int ArtistId { get; set; }
+        public string Name { get; set; }
+
+        //navigational properties.... this is the parent
+
+        public virtual ICollection<Album> Albums { get; set; }
+
+        
+    }
+}
